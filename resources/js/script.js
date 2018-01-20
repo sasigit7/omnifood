02f24fae2b4a4c6ac3698eef3fa63b36cc7fb886
shashi <sasitech18@gmail.com>
2018-01-20 21:44:52 +0000

    Making the navigation responsive

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-
     /* For the sticky navigation */
     $('.js--section-features').waypoint(function (direction) {
         if (direction == 'down') {
@@ -15,7 +14,6 @@ $(document).ready(function () {
 }, {
   offset: '25%'
 })  */
-
     /* Scroll on buttons */
     $('.js--scroll-to-plans').click(function () {
         $('html, body').animate({
@@ -27,14 +25,11 @@ $(document).ready(function () {
             scrollTop: $('.js--section-features').offset().top
         }, 1000);
     });
-
     /* Navigation scroll */
-
     $(function () {
         $('a[href*=#]:not([href=#])').click(function () {
             if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') ||
                 location.hostname == this.hostname) {
-
                 var target = $(this.hash);
                 target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
                 if (target.length) {
@@ -46,31 +41,38 @@ $(document).ready(function () {
             }
         });
     });
-
     /* Animations on scroll */
     $('.js--wp-1').waypoint(function (direction) {
         $('.js--wp-1').addClass('animated fadeIn');
     }, {
         offset: '50%'
     });
-
     $('.js--wp-2').waypoint(function (direction) {
         $('.js--wp-2').addClass('animated fadeInUp');
     }, {
         offset: '50%'
     });
-
     $('.js--wp-3').waypoint(function (direction) {
         $('.js--wp-3').addClass('animated fadeIn');
     }, {
         offset: '50%'
     });
-
     $('.js--wp-4').waypoint(function (direction) {
         $('.js--wp-4').addClass('animated pulse');
     }, {
         offset: '50%'
     });
-
-
+    /* Mobile Navigation */
+    $('.js--nav-icon').click(function () {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+        nav.slideToggle(200);
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+    });
 });
